7bdee62f8fb06dcb56f951ed369bde8ea0f6b538
fern <91796178+fern1991p@users.noreply.github.com>
2021-11-04 15:27:14 +0100

    Add files via upload
    trump game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// score 0 
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 canvas.style.border = '2px solid black';
@@ -19,6 +21,10 @@ let fake = new Image();
 fake.src = 'fake.png'
 let cloud = new Image();
 cloud.src = "clouds.png";
+let putin = new Image();
+putin.src = "putin.png"
+let kim = new Image();
+kim.src = "kim.png"
 let musicObj = new Audio();
 musicObj.src = "fakenews.mp3"
 let musicBackground = new Audio();
@@ -38,29 +44,40 @@ let decObj = 3.5
 let gap = 20;
 let start = false;
 let score = 0;
-
+let kimShow = false;
+let kimTime = 0
+let putinShow = false;
+let putinTime = 0
 
 let obj = [
     {x: 60, y: objY, img: fake},
-    {x: 100, y: objY + 300, img: vac},
+    {x: 100, y: objY + 300, img: fake},
     {x: 230, y: objY - 300, img: vac},
     {x: 300, y: objY - 200, img: vac},
     {x: 425, y: objY - 240, img: vac},
-    {x: 550, y: objY - 360, img: vac},
-    {x: 600, y: objY + 400, img: vac},
-    {x: 380, y: objY - 200, img: vac},
-    {x: 425, y: objY - 240, img: vac},
-    {x: 500, y: objY - 360, img: vac}
+    // {x: 550, y: objY - 360, img: vac},
+    // {x: 600, y: objY + 400, img: vac},
+    // {x: 380, y: objY - 200, img: vac},
+    // {x: 425, y: objY - 240, img: vac},
+    // {x: 500, y: objY - 360, img: vac}
 ]
 
 let clouds = [
     {x: 10, y: 10, img: cloud},
-    {x: 30, y: 50, img: cloud},
-    {x: 120, y: 35, img: cloud},
-    {x: 250, y: 15, img: cloud},
+    {x: 230, y: 65, img: cloud},
+    {x: 420, y: 20, img: cloud},
+    {x: 550, y: 15, img: cloud},
 ]
 
+let kimArr = [
+   {x: 750, y: 475}
+]
+let putinArr = [
+    {x: 750, y: 300}
+ ]
+
 function animation(){
+
 
     ctx.drawImage (gs, 0, 0)
     ctx.drawImage(trump, trumpX, trumpY, trumpWidth, trumpHeight)
@@ -88,19 +105,24 @@ function animation(){
         previousX = obj[i].x
         previousY = obj[i].y
         }  
-        
+        // Collision fake news -----
         if (obj[i].img === fake && trumpX + trumpWidth > obj[i].x && trumpX < obj[i].x + 60 && obj[i].y + 60 >= canvas.height - trumpHeight){
             score++
+            kimTime++
+            putinTime++
             obj[i].x = Math.floor(Math.random()* (canvas.width - 60))
             obj[i].y = - Math.floor(Math.random()* (canvas.width/2))
 
         }
         if (obj[i].img === fake && trumpX < obj[i].x && trumpX + trumpWidth > obj[i].x && obj[i].y + 60 >= canvas.height - trumpHeight){
             score++
+            kimTime++
+            putinTime++
             obj[i].x = Math.floor(Math.random()* (canvas.width - 60))
             obj[i].y = - Math.floor(Math.random()* (canvas.width/2))
         }
-
+        
+        // Collision Vaccin -----
         if (obj[i].img === vac && trumpX + trumpWidth > obj[i].x && trumpX < obj[i].x + 60 && obj[i].y + 60 >= canvas.height - trumpHeight){
             gameOver = true
             
@@ -109,38 +131,78 @@ function animation(){
         }
         if (obj[i].img === vac && trumpX < obj[i].x && trumpX + trumpWidth > obj[i].x && obj[i].y + 60 >= canvas.height - trumpHeight){
             gameOver = true
-           
-
         }
 
         if (score == 3){
-            console.log("it works")
-            decObj = 5
+            decObj = 5 
         }
+
         if (score == 6){
-            console.log("it works")
             decObj = 6
         }
+
         
     }
+
 
 for (i=0; i<clouds.length; i++){
     ctx.drawImage(cloud, clouds[i].x, clouds[i].y) 
     clouds[i].x = clouds[i].x + decObj 
     
-
-       if (clouds[i].x + 60 > canvas.width){
-         clouds[i].x = 0
-        clouds[i].y = 30
-
+        if (clouds[i].x + 60 > canvas.width){
+        clouds[i].x =  0
+        clouds[i].y =  Math.floor(Math.random()* (canvas.width/4))
       }
 
 }
 
 
 
-    ctx.font = '24px Verdana'
-    ctx.fillText(`Score: ${score}`, 30, canvas.height - 40 )
+
+
+
+
+    
+    if (kimTime % 9 === 0 && kimTime > 2){
+        kimShow = true
+    }
+    if (kimShow){ 
+        for (i=0; i < kimArr.length; i++){
+            ctx.drawImage(kim, kimArr[i].x, kimArr[i].y, 273, 222) 
+            kimArr[i].x = kimArr[i].x - 2 
+            if (kimArr[i].x + 273 <= 0 ){
+                kimShow = false
+                kimArr[i].x = 750
+                kimTime = 0
+           } 
+    
+    }
+
+}
+
+        if (putinTime % 4 === 0 && putinTime > 2){
+            putinShow = true
+        }
+        if (putinShow){ 
+            for (i=0; i < putinArr.length; i++){
+                ctx.drawImage(putin, putinArr[i].x, putinArr[i].y, 188, 263) 
+                putinArr[i].x = putinArr[i].x - 2
+                if (putinArr[i].x + 273 <= 0 ){
+                    putinShow = false
+                    putinArr[i].x = 750
+                    putinTime = 0
+            } 
+
+        }
+
+        }
+
+
+
+
+
+    ctx.font = '27px Verdana'
+    ctx.fillText(`Score: ${score}`, 30, canvas.height - 650 )
 
 if (gameOver){
     cancelAnimationFrame(intervalId);
@@ -148,21 +210,14 @@ if (gameOver){
     musicBackground.pause()
     musicObj.play()
     musicObj.volume = 0.2
-
-    
-    
-   
-}
+    decObj = 3.5
+    kimTime = 0    
+    }
 
 else{
-    console.log("gameover is NOT true")
     intervalId = requestAnimationFrame(animation);
+    }
 }
-
-
-
-}
-
 
 
 function handleStart(){
@@ -172,32 +227,45 @@ function handleStart(){
     musicBackground.play()
     musicBackground.volume = 0.1
     musicBackground.loop = true;
-    
-
 }
 
 function restart(){
-    console.log("restart")
     score = 0;
+    kimTime = 0;
+    putinTime = 0;
     gameOver = false;
     obj = [
         {x: 60, y: objY, img: fake},
-        {x: 100, y: objY + 300, img: vac},
+        {x: 100, y: objY + 300, img: fake},
         {x: 230, y: objY - 300, img: vac},
         {x: 300, y: objY - 200, img: vac},
         {x: 425, y: objY - 240, img: vac},
-        {x: 550, y: objY - 360, img: vac},
-        {x: 600, y: objY + 400, img: vac},
-        {x: 380, y: objY - 200, img: vac},
-        {x: 425, y: objY - 240, img: vac},
-        {x: 500, y: objY - 360, img: vac}
+        // {x: 550, y: objY - 360, img: vac},
+        // {x: 600, y: objY + 400, img: vac},
+        // {x: 380, y: objY - 200, img: vac},
+        // {x: 425, y: objY - 240, img: vac},
+        // {x: 500, y: objY - 360, img: vac}
     ]
+    let clouds = [
+        {x: 10, y: 10, img: cloud},
+        {x: 230, y: 65, img: cloud},
+        {x: 420, y: 20, img: cloud},
+        {x: 550, y: 15, img: cloud},
+    ]
+    
+    let kimArr = [
+       {x: 750, y: 475}
+    ]
+    let putinArr = [
+        {x: 750, y: 300}
+     ]
     handleStart()
 }
 
 
 window.addEventListener('load', () => {
     ctx.drawImage (bg, 0, 0)
+    startBtn.style.display = "none"
     restartBtn.style.display = "none";
 
     document.addEventListener('keydown', (event) => {
@@ -225,14 +293,7 @@ window.addEventListener('load', () => {
     if (event.key == "Enter" && gameOver == true){
     restart()
 
-    }    
-       
+    }     
     })
-
-
-            
-    
-    
-
-
 })
+
